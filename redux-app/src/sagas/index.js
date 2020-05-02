bd1dae79acbe9cbc3cd13c7bf14662161acdd9e1
archalwar <amarnath.archalwar@gmail.com>
2020-05-02 08:41:41 +0530

    sort is done

--- redux-app/src/sagas/index.js
+++ redux-app/src/sagas/index.js
@@ -1,25 +1,22 @@
-import { all, call, put, takeEvery } from 'redux-saga/effects';
-import { LOAD_TODO_LIST, RENDER_TODO_LIST } from '../actions';
+import { all, call, put, takeEvery } from "redux-saga/effects";
+import { LOAD_TODO_LIST, RENDER_TODO_LIST } from "../actions";
 
-
-
-export function* fetchToDoList() {  
+export function* fetchToDoList() {
   //let BASE_URL = 'https://jsonplaceholder.typicode.com/todos';
-  let BASE_URL = 'https://redux-app-dd2f9.firebaseio.com/todos.json';
+  let BASE_URL = "https://redux-app-dd2f9.firebaseio.com/todos.json";
   const response = yield call(fetch, BASE_URL, {
-    method: "GET"
+    method: "GET",
   });
   const data = yield response.json();
 
-  let val =Object.entries(data);
+  let val = Object.entries(data);
 
-   let newValues = val.map(singleval => (      
-      singleval[1]
-  ))
+  let newValues = val.map((singleval) => singleval[1]);
+  newValues.sort(function (a, b) {
+    return b.id - a.id;
+  });
 
   yield put({ type: RENDER_TODO_LIST, toDoList: newValues });
-  
-
 }
 
 export function* loadToDoList() {
